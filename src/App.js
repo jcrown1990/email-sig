123656e8f2d6ce35f489101567098ebe66a6b697
Branden Schrenk <branden.schrenk@ge.com>
2021-06-17 10:43:44 -0400

    Update to function component

--- src/App.js
+++ src/App.js
@@ -1,155 +1,137 @@
-import React, { Component } from 'react';
+import React, { useState, createRef } from 'react';
 import EmailSignature from './EmailSignature.jsx';
 import logo from './images/GElogo.svg';
 import './App.css';
 
-class App extends Component {
+function App() {
+  const [name, setName] = useState('Jane Smith');
+  const [pronoun, setPronoun] = useState('(she/her)');
+  const [mobileNumber, setMobileNumber] = useState('+1 234 567 8901');
+  const [teleNumber, setTeleNumber] = useState('+1 234 567 8900');
+  const [industry, setIndustry] = useState('GE Aviation');
+  const [family, setFamily] = useState('Commercial Engines');
+  const [jobTitle, setJobTitle] = useState('Engineer');
+  const [copied, setCopied] = useState(false);
 
-  constructor(props) {
-    super(props);
+  const iframe = createRef();
 
-    this.state = {
-      // Email signature form.
-      name: 'Jane Smith',
-      pronoun: '(she/her)',
-      mobileNumber: '+1 234 567 8901',
-      teleNumber: '+1 234 567 8900',
-      jobTitle: 'Engineer',
-      industry: 'GE Aviation',
-      family: 'Commercial Engines',
-      copied: false,
-    };
-
-    this.iframe = React.createRef();
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeTitle = this.handleChangeTitle.bind(this);
-    this.handleMobile= this.handleMobile.bind(this);
-    this.handleTele= this.handleTele.bind(this);
-    this.handlePronoun= this.handlePronoun.bind(this);
-    this.handleIndustry= this.handleIndustry.bind(this);
-    this.handleFamily= this.handleFamily.bind(this);
-    this.handleClickCopy= this.handleClickCopy.bind(this);
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  }
+  const handleFamily = (event) => {
+    setFamily(event.target.value);
   }
 
-  handleChangeName(event) {
-    this.setState({ name: event.target.value });
-  }
-  handleFamily(event) {
-    this.setState({ family: event.target.value });
+  const handleChangeTitle = (event) => {
+    setJobTitle(event.target.value);
   }
 
-  handleChangeTitle(event){
-    this.setState({ jobTitle: event.target.value});
+  const handleMobile = (event) => {
+    setMobileNumber(event.target.value);
   }
 
-  handleMobile(event){
-    this.setState({ mobileNumber: event.target.value});
+  const handleTele = (event) => {
+    setTeleNumber(event.target.value);
   }
 
-  handleTele(event){
-    this.setState({ teleNumber: event.target.value});
+  const handlePronoun = (event) => {
+    setPronoun(event.target.value);
   }
 
-  handlePronoun(event){
-    this.setState({ pronoun: event.target.value});
-  }
-  handleIndustry(event){
-    this.setState({ industry: event.target.value});
-  }
-  getIframe() {
-    return this.iframe.current.iframe.current.contentDocument;
+  const handleIndustry = (event) => {
+    setIndustry(event.target.value);
   }
 
-  handleClickCopy(event) {
+  const getIframe = () => {
+    return iframe.current.iframe.current.contentDocument;
+  }
+
+  const handleClickCopy = (event) => {
     event.preventDefault();
 
-     const doc = this.getIframe();
+    const doc = getIframe();
 
-     console.log(doc);
-    
-     doc.execCommand("selectAll");
-     doc.execCommand("copy");
+    console.log(doc);
 
-     this.setState({'copied': true });
+    doc.execCommand("selectAll");
+    doc.execCommand("copy");
+
+    setCopied(true);
   }
 
-  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
 
-    const { copied } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-
-          <div className="filter"></div>
-          <div className="header-wrapper">
-            <div>
-            <img src={logo} alt="GE Logo"/>
+        <div className="filter"></div>
+        <div className="header-wrapper">
+          <div>
+            <img src={logo} alt="GE Logo" />
             <h1 className="App-title">Email Signature Builder</h1>
-            </div>
-          </div>
-          <ul>
-            <li>1. Fill in your details</li>
-            <li>2. Click Copy</li>
-            <li>4. Open Outlook Signatures paste and apply</li>
-            <li>5. Done! </li>
-          </ul>
-        </header>
-        <div className="line-one"></div>
-        <div className="App-intro">
-          <div className="field-wrapper">
-            <label>First and Last Name</label>
-            <input value={this.state.name} onChange={this.handleChangeName}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Preferred pronouns</label>
-            <input value={this.state.pronoun} onChange={this.handlePronoun}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Job Title</label>
-            <input value={this.state.jobTitle} onChange={this.handleChangeTitle}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Customer Solution or Product Family</label>
-            <input value={this.state.family} onChange={this.handleFamily}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Telephone Number</label>
-            <input value={this.state.teleNumber} onChange={this.handleTele}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Mobile Number</label>
-            <input value={this.state.mobileNumber} onChange={this.handleMobile}/>
-          </div>
-          <div className="field-wrapper">
-            <label>Industry Brand</label>
-            <input value={this.state.industry} onChange={this.handleIndustry}/>
           </div>
         </div>
-        <div className="signature-container">
-
-          <EmailSignature
-            name={this.state.name}
-            mobileNumber={this.state.mobileNumber}
-            teleNumber={this.state.teleNumber}
-            jobTitle={this.state.jobTitle}
-            pronoun={this.state.pronoun}
-            industry={this.state.industry}
-            family={this.state.family}
-            ref={this.iframe}
-            />
-
-            <button
-            className="button copyButton"
-            onClick={this.handleClickCopy}
-            onBlur={this.handleBlurCopy}
-            >
-          { copied ? 'Copied!' : 'Copy' }
-          </button>
+        <ul>
+          <li>1. Fill in your details</li>
+          <li>2. Click Copy</li>
+          <li>4. Open Outlook Signatures paste and apply</li>
+          <li>5. Done! </li>
+        </ul>
+      </header>
+      <div className="line-one"></div>
+      <div className="App-intro">
+        <div className="field-wrapper">
+          <label>First and Last Name</label>
+          <input value={name} onChange={handleChangeName} />
+        </div>
+        <div className="field-wrapper">
+          <label>Preferred pronouns</label>
+          <input value={pronoun} onChange={handlePronoun} />
+        </div>
+        <div className="field-wrapper">
+          <label>Job Title</label>
+          <input value={jobTitle} onChange={handleChangeTitle} />
+        </div>
+        <div className="field-wrapper">
+          <label>Customer Solution or Product Family</label>
+          <input value={family} onChange={handleFamily} />
+        </div>
+        <div className="field-wrapper">
+          <label>Telephone Number</label>
+          <input value={teleNumber} onChange={handleTele} />
+        </div>
+        <div className="field-wrapper">
+          <label>Mobile Number</label>
+          <input value={mobileNumber} onChange={handleMobile} />
+        </div>
+        <div className="field-wrapper">
+          <label>Industry Brand</label>
+          <input value={industry} onChange={handleIndustry} />
         </div>
       </div>
-    );
-  }
+      <div className="signature-container">
+        <EmailSignature
+          name={name}
+          mobileNumber={mobileNumber}
+          teleNumber={teleNumber}
+          jobTitle={jobTitle}
+          pronoun={pronoun}
+          industry={industry}
+          family={family}
+          ref={iframe}
+        />
+
+        <button
+          className="button copyButton"
+          onClick={handleClickCopy}
+        >
+          {copied ? 'Copied!' : 'Copy'}
+        </button>
+      </div>
+    </div>
+  );
 }
+
+
+
 
 export default App;
